616303c75886f390cad5c4f6f039991af79de76f
Melissandre <magda6.06@wp.pl>
2015-12-12 12:23:41 +0100

    #1 CLEAN

--- DroolsTest.java
+++ DroolsTest.java
@@ -12,37 +12,24 @@ import org.drools.logger.KnowledgeRuntimeLogger;
 import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
 
-import java.awt.Component;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.List;
-import java.util.ArrayList;
-
-import javafx.scene.control.ComboBox;
-
-import javax.swing.*;
-
-
 /**
  * This is a sample class to launch a rule.
  */
 public class DroolsTest {
 	
-	public static String kotek;
+	public static String chosen;
 
     public static final void main(String[] args) {
-        try {
+        OknoGlowne okno = new OknoGlowne();
+        okno.setVisible(true);
+    }
+    
+    public void startDrools() {
+    	try {
             // load up the knowledge base
             KnowledgeBase kbase = readKnowledgeBase();
             StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
             KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
-            // go !
-            //Message message = new Message();
-            //message.setMessage("Hello World");
-            //message.setStatus(Message.HELLO);
-            //ksession.insert(message);
             DisplayFrame disp = new DisplayFrame();
             (new Thread(disp)).start();
             ksession.insert(disp);
@@ -79,50 +66,6 @@ public class DroolsTest {
     	public String nazwa;
     	public Chosen (String nazwa) {
     		this.nazwa = nazwa;
-    	}
-    }
-    
-    public static class DisplayFrame implements Runnable{
-    	
-    	public DisplayFrame() {
-    		System.out.println("Utworzono displayFrame");
-    	}
-    	
-    	public void setQuestionGoal (String nazwa) {
-    		NewJFrame frame = new NewJFrame(nazwa,new String[] {});
-    		frame.setVisible(true);
-    	}
-    	
-    	public void setQuestion2 (String nazwaPytania, String[] lista) {
-    		NewJFrame frame = new NewJFrame(nazwaPytania,lista);
-    		frame.setVisible(true);
-    		while(frame.counter < 1) {
-    			waiting(100);
-    		}
-    	}
-    	
-    	public void setQuestionB (String nazwaPytania, String[] lista) {
-    		FrameWithButtons fr = new FrameWithButtons();
-    		fr.wyswietl(nazwaPytania, lista);
-    		while(fr.counter < 1) {
-    			waiting(100);
-    		}
-    	}
-    	
-    	public String getKotek() {
-    		return DroolsTest.kotek;
-    	}
-    	
-    	public void waiting(int miliseconds){
-
-        	try {
-        	Thread.sleep( miliseconds);
-        	}
-        	catch (InterruptedException e) {}	
-        	}
-    	
-    	public void run() {
-    		
     	}
     }
 }
